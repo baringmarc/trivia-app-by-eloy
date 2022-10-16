1506dc4b1f1f1ed693ece1820f6321a6bfd846e8
Chieko.Bayron <danylchieko@gmail.com>
2022-10-16 18:34:48 +0800

    wala ragud

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,11 @@
 const Footer = () => {
-    return (
-        <div className = "footer">
-            <div className="signature">
-                <h3>Made by Marc Baring</h3>
-            </div>
-            <div className="credits">
-                <h4>API by the-trivia-api.com</h4>
-            </div>
-        </div>
-      );
-}
- 
+  return (
+    <div className="footer">
+      <div className="credits">
+        <h4>API by the-trivia-api.com</h4>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
